749bf5dbb8efbe2e31a2538dc0c809f369e5966a
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-09-30 15:37:58 +0200

    added. progressController and progressRouter

---
+++ backend/controllers/progressController.js
@@ -0,0 +1,3 @@
+const progressController = {}
+
+module.exports = progressController;
---
+++ backend/routers/progressRouter.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const progressController = require('../controllers/progressController')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send({message: "progress route funzionante"})
+})
