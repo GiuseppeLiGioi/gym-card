587728890dff3fe1a09e811e0de92beca2918319
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-09-30 13:51:48 +0200

    added. Basic middleware for static images, frontend requests, and json for req.body

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 require('dotenv').config();
 const conncetion = require('./data/db')
 const port = process.env.DB_PORT || 5000;
-
 const express = require('express')
-
 const app = express()
+
+//middleware generali
+app.use(cors({
+    origin: process.env.FRONTEND_APP
+}))
+app.use(express.static('public'));
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send("server attivo e pronto")
 })
 
 app.listen(port, () => {
-    console.log(`Server in ascolto sulla porta: ${port}`)
+    console.log(`Server in ascolto sulla porta: ${port}`) 
 })
