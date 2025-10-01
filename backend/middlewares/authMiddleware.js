a3939d066ded9c5e311086f6b1c6396b9f8f486b
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-01 12:27:55 +0200

    create. middleware for token route protection

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require ('jsonwebtoken')
+
+function authMiddleware(req, res, next){
+
+const authHeader = req.headers['authorization'];
+
+const token = authHeader && authHeader.split(' ')[1];
+
+if(!token) return res.status(401).json({error: "Token mancante o non valido"})
+
+jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if(err) return res.status(403).json({error: "Token non valido o scaduto"})
+
+    
+
+     req.user = user;
+     
+     next();
+
+
+})
+} 
+module.exports = authMiddleware;
