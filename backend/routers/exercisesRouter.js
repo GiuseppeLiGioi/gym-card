ce4594059ff7278faa3ae68622d6f8b5a5262046
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-02 11:39:25 +0200

    delete. exercisesController

--- backend/routers/exercisesRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const authMiddleware = require ('../middlewares/authMiddleware')
-const exercisesController = require('../controllers/exercisesController')
-
-const router = express.Router()
-
-module.exports = router
