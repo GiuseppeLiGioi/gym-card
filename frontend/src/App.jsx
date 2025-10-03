acd30d7b9ac4ca589742635abc34d2ba076c68f3
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-03 11:39:49 +0200

    added. react-router-dom and and did the page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import AuthenticationPage from "./assets/Pages/AuthenticationPage"
+import HomePage from "./assets/Pages/HomePage"
+import ProgressPage from "./assets/Pages/ProgressPage"
+import SingleSheetPage from "./assets/Pages/SingleSheetPage"
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<AuthenticationPage />}/>
+      <Route path="/home" element={<HomePage />}/>
+      <Route path="/sheet/:sheetId" element={<SingleSheetPage />}/>
+      <Route path="/sheet/:sheetId/progress" element={<ProgressPage />}/>
+    </Routes>
+    </BrowserRouter>
    
     </>
   )
