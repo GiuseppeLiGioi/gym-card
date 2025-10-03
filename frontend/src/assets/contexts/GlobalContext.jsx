2f2e58154c5978c2e5c7c0cded6f47658f87121e
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-03 17:34:42 +0200

    added. login and logout function at GlobalContext

--- frontend/src/assets/contexts/GlobalContext.jsx
+++ frontend/src/assets/contexts/GlobalContext.jsx
@@ -1,31 +1,80 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import {useNavigate} from "react-router-dom"
 
 const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
-  const [user, setUser] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
+    const [token, setToken] = useState(null);
+    const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate()
+
+
+    useEffect(() => {
+        const savedToken = localStorage.getItem("gym_token")
+        const savedUser = localStorage.getItem("gym_user")
+
+        if (savedToken) {
+            setIsAuthenticated(true)
+            setToken(savedToken)
+            setUser(savedUser)
+        }
+
+        setLoading(false)
+    }, [])
+
+    async function login(email, password) {
+        try {
+            const res = await fetch('http://localhost:5000/auth/login', {
+                method: POST,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password })
+            })
+
+            const data = await res.json()
+            if (!res.ok) throw new Error(data.message || "Errore Login")
+
+            setIsAuthenticated(true)
+            setToken(data.token)
+            setUser(data.user)
+
+            localStorage.setItem("gym_token", data.token)
+            localStorage.setItem("gym_user", JSON.stringify(data.user))
+        } catch (error) {
+            console.error("Errore login:", error.message);
+            return false;
+        }
+    }
+
+    async function logout() {
+        setIsAuthenticated(false)
+        setToken(null)
+        setUser(null)
+
+        localStorage.removeItem("gym_token")
+        localStorage.removeItem("gym_user")
+        navigate('/login')
+       
+    }
 
 
 
-
-  return (
-    <GlobalContext.Provider
-      value={{
-        token,
-        setToken,
-        user,
-        setUser,
-        isAuthenticated,
-        setIsAuthenticated,
-        loading,
-        setLoading
-      }}
-    >
-      {children}
-    </GlobalContext.Provider>
-  );
+    return (
+        <GlobalContext.Provider
+            value={{
+                token,
+                setToken,
+                user,
+                setUser,
+                isAuthenticated,
+                setIsAuthenticated,
+                loading,
+                setLoading
+            }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    );
 };
 export const useGlobalContext = () => useContext(GlobalContext);
