663653acca80ee5b84395dfe3a38de1479ebcd6c
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-04 16:25:56 +0200

    added. logic for managing login, success and error.

--- frontend/src/assets/Pages/AuthenticationPage.jsx
+++ frontend/src/assets/Pages/AuthenticationPage.jsx
@@ -1,40 +1,50 @@
 import { useNavigate } from "react-router-dom"
 import { useGlobalContext } from "../contexts/GlobalContext"
 import { useState } from "react"
-import {toast} from "react-toastify"
-export default function AuthenticationPage(){
-const {login} = useGlobalContext()
-const [email, setEmail] = useState("")
-const [password, setPassword] = useState("")
+import { toast } from "react-toastify"
+export default function AuthenticationPage() {
+    const { login } = useGlobalContext()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
 
-const navigate = useNavigate()
+    const navigate = useNavigate()
 
-async function handleLogin(){
-    const success = await login(email, password);
-    if(success){
-    toast.success("Login effettuato con successo")
-     navigate('/home')
-    }else{
-    toast.error("Credenziali errate")
-    }
+    async function handleLogin() {
+        setLoading(true)
+        
+        try {
+            const success = await login(email, password)
+            if (success) {
+                toast.success("Login effettuato con successo")
+                navigate("/home")
+            } else {
+                toast.error("Credenziali errate")
+            }
+            }catch (err) {
+            toast.error("Errore di connessione")
+            }finally {
+            setLoading(false)
+        }
+        
 }
-    return(
+    return (
         <div className="container-login-form">
-            <input 
-            type="text"
-            placeholder="Inserisci la tua e-mail"
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)}           
+            <input
+                type="text"
+                placeholder="Inserisci la tua e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
 
-            <input 
-            type="text"
-            placeholder="Inserisci la tua password"
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)}           
+            <input
+                type="text"
+                placeholder="Inserisci la tua password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
 
-            <button className="btn-login" onClick={handleLogin}>Accedi</button>
+            <button className="btn-login" onClick={handleLogin} disabled={loading}>{loading ? <div className="spinner"></div> : "Accedi"}</button>
 
         </div>
     )
