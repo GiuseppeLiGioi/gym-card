f5f3bee750e1ad5b0546f28630a8d6f1af78ff54
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-10-11 17:12:52 +0200

    added. structure of hero in CoursesPage

--- frontend/src/assets/Pages/Coursespage.jsx
+++ frontend/src/assets/Pages/Coursespage.jsx
@@ -1,5 +1,17 @@
-export default function CoursesPage(){
-    return(
-        <h2>sono Courses</h2>
+export default function CoursesPage() {
+
+    
+    return (
+        <section className="courses-hero">
+            <div className="hero-overlay">
+                <div className="hero-content">
+                    <h1 className="hero-title">Scopri i nostri corsi</h1>
+                    <p className="hero-subtitle">
+                        Scegli il percorso che fa per te e inizia subito a migliorarti.
+                    </p>
+                </div>
+            </div>
+        </section>
+
     )
 }
